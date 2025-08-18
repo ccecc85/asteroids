7e57d98dc8523a20221283293f8c8a8cbfc63e39
ccecc85 <ccecc85@gmail.com>
2025-08-18 19:45:43 +0200

    B: Triangle displayed

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-import pygame
+import pygame # type: ignore
+from player import Player
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids")
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player1 = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -17,7 +22,12 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
+        
+        player1.draw(screen)
+        
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000  # Convert milliseconds to seconds
 
 if __name__ == "__main__":
     main()
