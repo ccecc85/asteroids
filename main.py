94c51efbe34df32fad8951b9ad8bf769d64b022c
ccecc85 <ccecc85@gmail.com>
2025-08-19 08:36:48 +0200

    E: cleanup

--- main.py
+++ main.py
@@ -5,27 +5,35 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.set_caption("Asteroids")
     clock = pygame.time.Clock()
-    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
 
     player1 = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
 
+    dt = 0
+
+    pygame.display.set_caption("Asteroids")
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     while True:
+        # Handle events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
                 return
 
-        screen.fill((0, 0, 0))
+        # Update game state
+        updatable.update(dt)
         
-        player1.draw(screen)
-        player1.update(dt)
-        
+        # Draw everything
+        screen.fill(("black"))
+        for sprite in drawable:
+            sprite.draw(screen)
+
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000  # Convert milliseconds to seconds
